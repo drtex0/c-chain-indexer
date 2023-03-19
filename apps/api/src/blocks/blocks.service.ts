5e528e70ec9c1314e68bc6fc87e83a47e8fcf3b4
0xTex <tdza@mail.co>
2023-03-19 19:03:39 +0100

    feat(api): add documentation

--- apps/api/src/blocks/blocks.service.ts
+++ apps/api/src/blocks/blocks.service.ts
@@ -8,33 +8,6 @@ export class BlocksService {
   async getTotalBlocks(): Promise<number> {
     const totalBlocks = await this.prismaService.block.count();
 
-    const dups = await this.prismaService.$runCommandRaw({
-      aggregate: 'Block',
-      pipeline: [
-        {
-          $group: {
-            _id: '$id',
-            count: { $sum: 1 },
-          },
-        },
-        {
-          $match: {
-            count: { $gt: 1 },
-          },
-        },
-        {
-          $project: {
-            blockId: '$_id',
-            count: 1,
-            _id: 0,
-          },
-        },
-      ],
-      cursor: {},
-    });
-
-    console.log(JSON.stringify(dups));
-
     return totalBlocks;
   }
 }
